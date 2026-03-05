5feaa76f8d02248fd528ba9e7c8d50c64114bd83
isadeleke <idowu.adeleke@students.plymouth.ac.uk>
2026-03-05 19:37:05 +0000

    artofest backend

---
+++ artofest-backend/db/connection.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+let pool;
+
+if (process.env.DATABASE_URL) {
+  // Render / Production
+  pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  // Local development
+  pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT || 5432,
+  });
+}
+
+module.exports = pool;
