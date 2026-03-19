f37a652af8ce541095783abc5a22db1c37b8eb85
isadeleke <idowu.adeleke@students.plymouth.ac.uk>
2026-03-19 17:23:18 +0000

    fix auth file seperation

--- artofest-backend/routes/auth.js
+++ artofest-backend/routes/auth.js
@@ -6,30 +6,4 @@ const { register, login } = require('../controllers/authController');
 router.post('/register', register);
 router.post('/login', login);
 
-module.exports = router;const jwt = require('jsonwebtoken');
-
-const authMiddleware = (req, res, next) => {
-
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader) {
-    return res.status(401).json({ message: "No token provided" });
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  try {
-
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = decoded;
-
-    next();
-
-  } catch (error) {
-    return res.status(401).json({ message: "Invalid token" });
-  }
-
-};
-
-module.exports = authMiddleware;
+module.exports = router;
