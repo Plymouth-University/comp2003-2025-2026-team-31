fabe430a853c4fed01fd200813c68d3ae599d532
isadeleke <idowu.adeleke@students.plymouth.ac.uk>
2026-03-19 19:47:17 +0000

    updated getfestivalById fixed

--- artofest-backend/routes/festival.js
+++ artofest-backend/routes/festival.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getFestivals } = require('../controllers/festivalControllers');
+const { getFestivals, getFestivalById } = require('../controllers/festivalControllers');
 console.log('getFestivals is:', getFestivals);
 
 router.get('/', getFestivals);
